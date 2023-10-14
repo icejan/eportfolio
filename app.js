8b7d9c9449deb07be57d9a3915c0619a37a73185
Janice <janiceyezhu@gmail.com>
2023-10-14 02:31:52 -0400

    added dark-theme

---
+++ app.js
@@ -0,0 +1,9 @@
+var theme__icon = document.getElementById("theme__icon");
+    theme__icon.onclick = function() {
+        document.body.classList.toggle("dark-theme");
+        if(document.body.classList.contains("dark-theme")) {
+            theme__icon.className="fas fa-sun fa-sm";
+        } else {
+            theme__icon.className="fas fa-moon fa-sm";
+        }
+    }
